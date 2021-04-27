b9345677670e8b1a5f59b7ce752564a3c0c922f8
ccantu20 <carloscq920@gmail.com>
2021-04-27 13:06:23 -0500

    Setup the api routes

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.get('/', (req, res) => res.send('API running'))
+app.get('/', (req, res) => res.send('API running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen(PORT, () => {
     console.log(`Listening in on PORT ${PORT}!`)
