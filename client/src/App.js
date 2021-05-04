7223b2954634e4afa73385b389e5a423607e97b8
ccantu20 <carloscq920@gmail.com>
2021-05-04 15:18:26 -0500

    Setup a few of the routes in React

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
-import React, { Fragment } from 'react';
-import Navbar from '../src/components/layout/Navbar';
-import Landing from '../src/components/layout/Landing';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Profiles from './components/profiles/Profiles';
 import './App.css';
 
 function App() {
   return (
-    <Fragment>
+    <Router>
       <Navbar />
-      <Landing />
-    </Fragment>
+      <Route exact path ='/' >
+        <Landing />
+      </Route>
+      <Switch>
+        <Route exact path='/register'>
+          <Register />
+        </Route>
+        <Route exact path='/login'>
+          <Login />
+        </Route>
+        <Route exact path='/profiles'>
+          <Profiles />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,12 +4,12 @@ const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
             <h1>
-                <a href="index.html"><i className="fas fa-code"></i> DevLink</a>
+                <a href="/"><i className="fas fa-code"></i> DevLink</a>
             </h1>
             <ul>
-                <li><a href="profiles.html">Developers</a></li>
-                <li><a href="register.html">Register</a></li>
-                <li><a href="login.html">Login</a></li>
+                <li><a href="/profiles">Developers</a></li>
+                <li><a href="/register">Register</a></li>
+                <li><a href="/login">Login</a></li>
             </ul>
         </nav>
     )
