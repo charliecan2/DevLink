2df64e2b528cddec9bbf3bac115ad074d6405133
ccantu20 <carloscq920@gmail.com>
2021-05-20 14:32:01 -0500

    Created and implemented a reducer to handle retrieving user profiles

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Route, Redirect} from 'react-router-dom'
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
 const PrivateRoute =({ component: Component, auth: { isAuthenticated, loading},
 ...rest }) => (
@@ -18,4 +18,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps)(PrivateRoute)
+export default connect(mapStateToProps)(PrivateRoute);
